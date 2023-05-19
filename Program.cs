cc8d1dfa2664f2ba429f282878a89912734af432
STIJN DE SMET <STIJN.DESMET@vdabcampus.be>
2023-05-19 23:38:46 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -1,43 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics.Metrics;
-
-using (StreamReader file = new StreamReader("input.txt"))
+using testSpilberg;
+Console.WriteLine("give an input file:");
+Console.WriteLine("(default file will be input.txt)");
+string filePath = Console.ReadLine();
+if (filePath == String.Empty)
+{
+    filePath = "input.txt";
+}
+Console.Clear();
+Console.WriteLine("what length should the full words be?");
+string inputLength = Console.ReadLine();
+int length;
+if (inputLength == String.Empty)
+{
+    length = 6;
+}
+else
+{
+    length = Convert.ToInt32(inputLength);
+}
+using (StreamReader file = new(filePath))
 {
     string ln;
-    List<string> lines = new List<string>();
-    List<string> foundWords = new List<string>();
+    List<Word> words = new();
+    List<string> parts = new();
     while ((ln = file.ReadLine()) != null)
     {
-        if (!lines.Contains(ln))
-        lines.Add(ln);
-    }
-    file.Close();
-    Console.WriteLine("program is checking for possible combinations.");
-    for (int i = 0; i < lines.Count; i++)
-    {
-        string line = lines[i];
-        for (int y = 0; y < lines.Count; y++)
+        if (ln.Length == length)
         {
-            string line2 = lines[y];
-            string currentWord = line + line2;
-            if (currentWord.Length == 6 && !foundWords.Contains(currentWord))
-            {
-                if (lines.Contains(currentWord))
-                {
-                    foundWords.Add(currentWord);
-
-                }
-            }
+            Word word = new Word();
+            word.FullWord = ln;
+            if (!words.Contains(word))
+                words.Add(word);
         }
-
+        else if (ln.Length < length && !parts.Contains(ln))
+        {
+            parts.Add(ln);
+        }
     }
+    Console.WriteLine("program is checking for possible combinations.");
+    foreach (Word item in words)
+    {
+        item.FindWordCombinations(parts);
+    }
+
     Console.WriteLine("Program has finished, these are the found words in the input file with combinations.");
     int counter = 0;
-    foreach (string word in foundWords)
+    words.OrderBy(x => x.FullWord);
+    foreach (Word word in words)
     {
-        Console.Write(word + " ");
+        if (word.Word1 == null || word.Word2 == null)
+            return;
+        Console.Write(word.FullWord + " : " + word.Word1 + " + " + word.Word2);
         counter++;
-        if (counter == 10)
+        if (counter == 1 )
         {
             counter = 0;
             Console.WriteLine();
---
+++ Word.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testSpilberg
+{
+    public class Word
+    {
+        public Word()
+        {
+
+        }
+
+        public string FullWord { get; set; }
+        public string Word1 { get; set; }
+        public string Word2 { get; set; }
+
+        public void FindWordCombinations(List<string> words)
+        {
+            for (int i = 0; i < words.Count; i++)
+            {
+                for (int j = i + 1; j < words.Count; j++)
+                {
+                    if (words[i] + words[j] == FullWord)
+                    {
+                        Word1 = words[i];
+                        Word2 = words[j];
+                        return; // Found a valid combination, exit the function
+                    }
+                }
+            }
+        }
+    }
+}
