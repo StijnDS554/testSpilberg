2eacaa9c9db52c93da91da73db23231f2696eb3e
STIJN DE SMET <STIJN.DESMET@vdabcampus.be>
2023-05-22 12:16:59 +0200

    update

--- Program.cs
+++ Program.cs
@@ -4,14 +4,18 @@ using testSpilberg;
 Console.WriteLine("give an input file:");
 Console.WriteLine("(default file will be input.txt)");
 string filePath = Console.ReadLine();
+
 if (filePath == String.Empty)
 {
     filePath = "input.txt";
 }
+
 Console.Clear();
 Console.WriteLine("what length should the full words be?");
+
 string inputLength = Console.ReadLine();
 int length;
+
 if (inputLength == String.Empty)
 {
     length = 6;
@@ -20,53 +24,61 @@ else
 {
     length = Convert.ToInt32(inputLength);
 }
+
 using (StreamReader file = new(filePath))
 {
     string ln;
-    List<Word> words = new();
+    WordList wordList = new();
     List<string> parts = new();
+
     while ((ln = file.ReadLine()) != null)
     {
         if (ln.Length == length)
         {
             Word word = new Word();
             word.FullWord = ln;
-            if (!words.Contains(word))
-                words.Add(word);
+            if (!wordList.words.Contains(word))
+                wordList.words.Add(word);
         }
         else if (ln.Length < length && !parts.Contains(ln))
         {
             parts.Add(ln);
         }
     }
+
+
     Console.WriteLine("program is checking for possible combinations.");
-    foreach (Word item in words)
+    foreach (Word item in wordList.words)
     {
         item.FindWordCombinations(parts);
     }
 
+    wordList.OrderList();
     Console.WriteLine("Program has finished, these are the found words in the input file with combinations.");
     int counter = 0;
-
-    Word lastResult = null;
-
-    foreach (Word word in words)
+    Word lastWord = new Word();
+    foreach (var word in wordList.words)
     {
-        if (lastResult != null && lastResult.FullWord == word.FullWord)
-            words.Remove(word);
+
+
+        if (word.Word1 == null || word.Word2 == null)
+        {
+            return;
+        }
+        else if (word.Word1 == lastWord.Word1)
+        {
+            
+        }
         else
         {
-            lastResult = word;
-        }
-        if (word.Word1 == null || word.Word2 == null)
-            return;
-
-        Console.Write(word.FullWord + " : " + word.Word1 + " + " + word.Word2);
-        counter++;
-        if (counter == 1 )
-        {
-            counter = 0;
-            Console.WriteLine();
+            lastWord = word;
+            Console.Write(word.FullWord + " : " + word.Word1 + " + " + word.Word2);
+            counter++;
+            if (counter == 1)
+            {
+                counter = 0;
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ WordList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testSpilberg
+{
+    public class WordList
+    {
+        public WordList()
+        {
+            words = new List<Word>();
+        }
+        private List<Word> Words { get; set; }
+        public List<Word> words { get { return Words; } set { Words = value; } }
+        public void OrderList()
+        {
+            var orderedList = words.OrderBy(x => x.FullWord);
+            Word lastResult = new Word();
+            List<Word> result = new List<Word>();
+            foreach (Word word in orderedList)
+            {
+                if (lastResult.Word1 == word.Word1 && lastResult.Word2 == word.Word2)
+                {
+                }
+                else if (word.Word1 == null || word.Word2 == null)
+                {
+                }
+                else
+                {
+
+                    result.Add(word);
+                }                
+            }
+            words = result;
+        }
+    }
+}
